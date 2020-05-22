7cb98df23bd1cc5d80733cf3da49725c91640c24
valter barros <valterinside@gmail.com>
2020-05-22 01:13:52 -0300

    master-fix-paths
    Signed-off-by: valter barros <valterinside@gmail.com>

--- serve.js
+++ serve.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("*", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/dist/views/index.html');
 });
 
 // listen for requests :)
