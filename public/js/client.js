6b09d77d01a43b67c32ac8efab3016adb06a36fa
valter barros <valterinside@gmail.com>
2020-05-23 16:57:51 -0300

    master
    Signed-off-by: valter barros <valterinside@gmail.com>

--- public/js/client.js
+++ public/js/client.js
@@ -129,7 +129,7 @@ TrelloPowerUp.initialize({
           t.card('id')
           .then((card) => {
             t.get('card', 'shared', card.id).then((prLink) => {
-              console.log(prLink)
+              console.log(card)
             })
           })
           return {
