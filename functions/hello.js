60fd0a3eb59a81a1daf4ede45caff1ff6b11523a
valter barros <valterinside@gmail.com>
2020-05-26 23:36:22 -0300

    patch/using-netlifly-functions
    Signed-off-by: valter barros <valterinside@gmail.com>

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = function(event, context, callback) {
+  console.log(callback);
+  
   callback(null, {
-  statusCode: 200,
-  body: "Hello, World"
+    statusCode: 200,
+    body: "Hello, World"
   });
 }
