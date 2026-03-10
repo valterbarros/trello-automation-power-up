357bd6453e2c1265306e4c9d6aafb18bf4a28241
Valter <valter.junior@clicksign.com>
2026-03-10 12:31:39 -0300

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -163,16 +163,16 @@ TrelloPowerUp.initialize({
       }
     ];
   },
-  // 'list-actions': function (t) {
-  //   return t.list('name', 'id')
-  //     .then(function (list) {
-  //       return [{
-  //         text: "Get List Stats",
-  //         callback: function (t) {
-  //           window.parent.postMessage({list, type: 'trello-extension-integration'})
-  //           console.log("list inside power up: ", list, window.parent.document)
-  //         }
-  //       }];
-  //     });
-  // }
+  'list-actions': function (t) {
+    return t.list('name', 'id')
+      .then(function (list) {
+        return [{
+          text: "Get List Stats",
+          callback: function (t) {
+            window.parent.postMessage({list, type: 'trello-extension-integration'})
+            console.log("list inside power up: ", list, window.parent.document)
+          }
+        }];
+      });
+  }
 });
